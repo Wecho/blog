38361db8935695072509571d918ddbec20fb3e61
Wecho <wangyaooay@163.com>
2017-09-04 23:42:20 +0800

    add Filter

--- blogcore/src/main/java/com/wecho/core/servlet/filter/TitleFilter.java
+++ blogcore/src/main/java/com/wecho/core/servlet/filter/TitleFilter.java
@@ -2,23 +2,16 @@ package com.wecho.core.servlet.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "Filter",urlPatterns = {"/*"},dispatcherTypes = {DispatcherType.REQUEST,DispatcherType.ERROR})
+@WebFilter(filterName = "Filter")
 public class TitleFilter implements javax.servlet.Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        req.setCharacterEncoding("utf-8");
-        resp.setCharacterEncoding("utf-8");
-        MyResponseWrapper myResponseWrapper = new MyResponseWrapper((HttpServletResponse) resp);
-        myResponseWrapper.setCharacterEncoding("utf-8");
-        chain.doFilter(req, myResponseWrapper);
-        String respStr = myResponseWrapper.getResult().replace("{title}","nihao");
-        resp.getOutputStream().write(
-                respStr.getBytes("utf-8"));
+        chain.doFilter(req, resp);
+
     }
 
     public void init(FilterConfig config) throws ServletException {
--- blogcore/src/main/java/com/wecho/core/testservlet/TestServlet.java
+++ blogcore/src/main/java/com/wecho/core/testservlet/TestServlet.java
@@ -6,17 +6,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
-@WebServlet(name = "/test")
+@WebServlet(name = "/")
 public class TestServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Pattern pattern = Pattern.compile("^([hH][tT]{2}[pP]://|[hH][tT]{2}[pP][sS]://)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~\\\\/])+$");
-        System.out.println(pattern.matcher("www.baidu.com"));
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=utf-8");
-        request.getRequestDispatcher("WEB-INF/view/main/header.jsp").forward(request,response);
+        response.setCharacterEncoding("utf-8");
+        request.getRequestDispatcher("WEB-INF/view/main/index.jsp").forward(request,response);
     }
 }
