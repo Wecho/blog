60640f2df86aeccbf3d3b7cc9b59eba9506db552
wecho <wecho@wecho.com>
2017-11-17 11:12:55 +0800

    test user

---
+++ blogcore/src/main/Test/com/wecho/core/domain/test/TestUser.java
@@ -0,0 +1,21 @@
+package com.wecho.core.domain.test;
+
+import com.wecho.core.domain.User;
+import com.wecho.core.spring.config.LoginConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= LoginConfig.class)
+public class TestUser {
+    @Autowired
+    User user;
+    @Test
+    public void testUser(){
+        Assert.assertNotNull(user);
+    }
+}
